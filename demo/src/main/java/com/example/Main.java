94bcb8145348b6f22ebdd0ccbb26e0d1153952e7
Carlos <cz090219@gmail.com>
2024-09-10 10:25:44 -0500

    cm1

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,54 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double numero1;
+        double numero2;
+        String operacion;
+
+        double resultado = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce la operación (+, -, *, /, %): ");
+        operacion = scanner.nextLine();
+
+        System.out.println("Ingrese el primer numero:");
+        numero1 = scanner.nextDouble();
+
+        System.out.println("ingrese el segundo numero:");
+        numero2 = scanner.nextDouble();
+
+     
+        
+        if (operacion == "+") {
+            resultado = numero1 + numero2;
+
+            if (operacion == "-") {
+                resultado = numero1 - numero2;
+
+                if (operacion == "*") {
+                    resultado = numero1 + numero2;
+
+                    if (operacion == "/") {
+                        resultado = numero1 / numero2;
+
+                        if (operacion == "%") {
+                            resultado = numero1 % numero2;
+
+                            if (operacion == "x") {
+                                
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+       
+
     }
 }
